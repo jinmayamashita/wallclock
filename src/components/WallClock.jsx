55cd5b69576a12b9ceff1fce9272e8d36bd3966d
Jinma Yamashita <jinmayamashita@gmail.com>
2020-01-21 00:05:42 +0900

    refactor: update getCurrentTime

--- src/components/WallClock.jsx
+++ src/components/WallClock.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import style from "./WallClock.scss";
+import { WallClockNumber } from "./WallClockNumber";
 
 const getCurrentTime = () => {
-  const tt = new Date();
-  const h = tt.getHours();
-  const m = tt.getMinutes();
-  const s = tt.getSeconds();
-  return [h, m, s].join(":");
+  const now = new Date();
+  const hou = now.getHours();
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+  return {
+    hou: hou.toString().padStart(2, "0"),
+    min: min.toString().padStart(2, "0"),
+    sec: sec.toString().padStart(2, "0")
+  };
 };
 
 // TODO: colours are not the only hexadecimal colours
@@ -17,24 +22,41 @@ const setBackgroundStyle = p => ({ backgroundColor: `#${p}` });
 const useQuery = () => new URLSearchParams(useLocation().search);
 
 export const WallClock = () => {
-  const [timer, setTimer] = React.useState(getCurrentTime());
+  const [time, setTime] = React.useState(getCurrentTime());
   const q = useQuery();
+
   const qColor = q.get("color") ?? "FFFFFF";
   const qBg = q.get("bg") ?? "FFC000";
 
   const color = setColorStyle(qColor);
-  const bgColor = setBackgroundStyle(qBg);
+  const backgroundColor = setBackgroundStyle(qBg);
 
   React.useEffect(() => {
     const change = () => {
-      setTimer(() => getCurrentTime());
+      setTime(x => ({ ...x, ...getCurrentTime() }));
     };
     setInterval(change, 1000);
-  }, [setTimer]);
+  }, [setTime]);
 
   return (
-    <div className={style.clockBackground}>
-      <h1 className={style.clockText}>{timer}</h1>
+    <div
+      className={style.clockBackground}
+      style={{
+        ...backgroundColor
+      }}
+    >
+      <div className={style.clock}>
+        <p
+          className={style.clockText}
+          style={{
+            ...color
+          }}
+        >
+          <WallClockNumber number={time.hou} />:
+          <WallClockNumber number={time.min} />:
+          <WallClockNumber number={time.sec} />
+        </p>
+      </div>
     </div>
   );
 };
